c5d31495ff297e72a2a17b4a4b37cdfaa3505c3d
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2023-04-08 17:16:05 -0300

    props

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,25 +1,19 @@
 import './App.css';
-import HelloWord from './components/HelloWorld'
+import SayMyName from './components/SayMyName'
+import Pessoa from './components/Pessoa';
 
+const name = 'Maria'
 function App() {
-
-  const name = 'Helton'
-  const newName = name.toUpperCase()
-
-  function sum(a, b){
-    return a + b
-  }
-
-  const url = 'https://via.placeholder.com/150'
-
   return (
     <div className="App">
-      <h1> Alterando o JSX</h1>
-      <p> olá {newName} </p>
-      <p> Multiplicação { 4 * 5 } </p>
-      <p> Soma: {sum(4, 4)} </p>
-      <img src={url} alt="Minha Imagem"/>
-      <HelloWord/>
+      <SayMyName nome='Teixeira'/>
+      <SayMyName nome={name}/>
+      <Pessoa 
+        nome="Rodrigo" 
+        idade="28" 
+        profissão="Programador" 
+        foto="https://via.placeholder.com"
+        />
     </div>
   );
 }
