86afaecb40dbab2cd3723b2d2befe1d5e2d2269b
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2023-04-14 00:24:52 -0300

    css no react

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import SayMyName from './components/SayMyName'
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 const name = 'Maria'
 function App() {
   return (
     <div className="App">
+      <Frase/>
+      <h1>Testando CSS</h1>
       <SayMyName nome='Teixeira'/>
       <SayMyName nome={name}/>
       <Pessoa 
